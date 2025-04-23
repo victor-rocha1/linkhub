be0c80dd5ef1ee6402912ca6bf3381db5c588d4e
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2025-04-23 09:53:17 -0300

    feat: add data.js file to store project data

---
+++ src/utils/data.js
@@ -0,0 +1,16 @@
+const data = {
+    title: "Victor Rocha",
+    subtitle: "Desenvolvedor",
+    links: [
+        { text: "GitHub", url: "https://github.com/victor-rocha1" },
+        { text: "Portifólio Web", url: "https://victor-rocha1.github.io/portifolio-victor2/" },
+        { text: "Linkdedin", url: "https://www.linkedin.com/in/victor-rocha-751376267/" },
+    ],
+    social:[
+        { icon: "", url: "" },
+        { icon: "", url: "" },
+        { icon: "", url: "" },
+    ]
+}
+
+export default data;
