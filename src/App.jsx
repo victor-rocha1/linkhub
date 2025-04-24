f9b443c7883de386b999529ad8cfc37e219f4c06
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2025-04-24 13:04:26 -0300

    feat: init css

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,16 @@ function App() {
   return (
     <main>
       <div>
+        <img src="../avatar.jpg" alt="avatar" />
         <h1>{data.title}</h1>
         <h2>{data.subtitle}</h2>
       </div>
 
-      <>
+      <div className="links-list">
         {data.links.map((item) => (
           <Link text={item.text} url={item.url} />
         ))}
-      </>
+      </div>
 
       <div className="description">
         <p>{data.desciption}</p>
